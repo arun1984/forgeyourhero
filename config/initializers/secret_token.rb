088d5471f4fa33582709273da839edfdb1c5a1a5
Arun <arun.1984@gmail.com>
2011-08-20 15:27:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forgeyourhero::Application.config.secret_token = 'e03299d3ef30bdc55a93a6a3d35aaa8b4ea362b4999d3e4b3f4f3176bc59e634df47371750ea532fb46224c9fa3382326e3d6db8cf896e28364fe578a1aac264'
